e7db01d4215df570e268b0db5b76bcf2816c957f
Ibrahim <creativeibi77@gmail.com>
2019-07-06 18:26:18 +1000

    bot error handling

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -37,87 +37,89 @@ class MyClient(discord.Client):
     async def send_messages(self):
         await self.wait_until_ready()
         while not self.is_closed():
-            while not self.recv_q.empty():
-                data = self.recv_q.get()
+            try:
+                while not self.recv_q.empty():
+                    data = self.recv_q.get()
 
-                if data["type"] == "message":
-                    if not "channel_name" in data: #if the channel name is not soecified, the id will be
-                        channel = self.get_channel(int(data.get("channel_id", self.indexed_channels["Super"]["bridge"])))
-                        await channel.send(data["message"])
-                    else:
-                        channel_id = self.indexed_channels[data["category_name"]][data["channel_name"]]
-                        channel = self.get_channel(int(channel_id))
-                        await channel.send(data["message"])
+                    if data["type"] == "message":
+                        if not "channel_name" in data: #if the channel name is not soecified, the id will be
+                            channel = self.get_channel(int(data.get("channel_id", self.indexed_channels["Super"]["bridge"])))
+                            await channel.send(data["message"])
+                        else:
+                            channel_id = self.indexed_channels[data["category_name"]][data["channel_name"]]
+                            channel = self.get_channel(int(channel_id))
+                            await channel.send(data["message"])
 
-                if data["type"] == "request":
-                    if data["request"] == "channels":
-                        snd_dat = {
-                            "type" : "answer",
-                            "request_id" : data["request_id"],
-                            "channels" : self.indexed_channels
-                        }
-                        self.send_q.put(snd_dat)
-                    
-                    if data["request"] == "users":
-                        usrs = self.gld.members
-                        users = {}
-                        roles = []
-                        for u in usrs:
-                            for r in u.roles:
-                                roles.append(str(r))
-                            users[u.id] = {
-                                "user_id" : u.id,
-                                "roles" : roles,
-                                "user_name" : u.name,
-                                "nick" : u.display_name,
-                                "mention" : u.mention,
-                                "avatar" : str(u.avatar_url)
-
+                    if data["type"] == "request":
+                        if data["request"] == "channels":
+                            snd_dat = {
+                                "type" : "answer",
+                                "request_id" : data["request_id"],
+                                "channels" : self.indexed_channels
                             }
+                            self.send_q.put(snd_dat)
+                        
+                        if data["request"] == "users":
+                            usrs = self.gld.members
+                            users = {}
+                            roles = []
+                            for u in usrs:
+                                for r in u.roles:
+                                    roles.append(str(r))
+                                users[u.id] = {
+                                    "user_id" : u.id,
+                                    "roles" : roles,
+                                    "user_name" : u.name,
+                                    "nick" : u.display_name,
+                                    "mention" : u.mention,
+                                    "avatar" : str(u.avatar_url)
 
-                        snd_dat = {
-                            "type" : "answer",
-                            "request_id" : data["request_id"],
-                            "users" : users
-                        }
-                        self.send_q.put(snd_dat)
-                
-                if data["type"] == "create_channel":
-                    category = self.indexed_channels[data["category_name"]]["category_id"]
-                    category = self.get_channel(int(category))
-                    if data["channel_type"] == "text":
-                        await category.create_text_channel(data["channel_name"])
-                    elif data["channel_type"] == "vc":
-                        await category.create_voice_channel(data["channel_name"])
+                                }
 
-                if data["type"] == "set_role":
-                    user = self.gld.get_member(int(data["user_id"]))
-                    role = None
-                    if "role_name" in data:
-                        role_id = int(self.indexed_roles[data["role_name"]])
-                        role = self.gld.get_role(role_id)
-                    else:
-                        role = self.gld.get_role(int(data["role_id"]))
-                    await user.add_roles(role)
-
-                if data["type"] == "clear_channel": #does not work
-                    ch = self.get_channel(int(data["channel_id"]))
-                    async for m in ch.history():
-                        await m.delete()
-                
-                if data["type"] == "replace_last":
-                    if not "channel_name" in data: #if the channel name is not soecified, the id will be
-                        ch = self.get_channel(int(data.get("channel_id", 566610532786765854)))
-                    else:
-                        channel_id = self.indexed_channels[data["category_name"]][data["channel_name"]]
-                        ch = self.get_channel(int(channel_id))
+                            snd_dat = {
+                                "type" : "answer",
+                                "request_id" : data["request_id"],
+                                "users" : users
+                            }
+                            self.send_q.put(snd_dat)
                     
-                    msg = ch.last_message
-                    if msg != None:
-                        await msg.edit(content=data["message"])
-                    else:
-                        await ch.send(data["message"])
-        
+                    if data["type"] == "create_channel":
+                        category = self.indexed_channels[data["category_name"]]["category_id"]
+                        category = self.get_channel(int(category))
+                        if data["channel_type"] == "text":
+                            await category.create_text_channel(data["channel_name"])
+                        elif data["channel_type"] == "vc":
+                            await category.create_voice_channel(data["channel_name"])
+
+                    if data["type"] == "set_role":
+                        user = self.gld.get_member(int(data["user_id"]))
+                        role = None
+                        if "role_name" in data:
+                            role_id = int(self.indexed_roles[data["role_name"]])
+                            role = self.gld.get_role(role_id)
+                        else:
+                            role = self.gld.get_role(int(data["role_id"]))
+                        await user.add_roles(role)
+
+                    if data["type"] == "clear_channel": #does not work
+                        ch = self.get_channel(int(data["channel_id"]))
+                        async for m in ch.history():
+                            await m.delete()
+                    
+                    if data["type"] == "replace_last":
+                        if not "channel_name" in data: #if the channel name is not soecified, the id will be
+                            ch = self.get_channel(int(data.get("channel_id", 566610532786765854)))
+                        else:
+                            channel_id = self.indexed_channels[data["category_name"]][data["channel_name"]]
+                            ch = self.get_channel(int(channel_id))
+                        
+                        msg = ch.last_message
+                        if msg != None:
+                            await msg.edit(content=data["message"])
+                        else:
+                            await ch.send(data["message"])
+            except exp as iden:
+                print("there was an error?", iden)
             await asyncio.sleep(0.1)
 
     async def on_member_join(self, member):
