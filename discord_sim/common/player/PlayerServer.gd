a312b5d7c15f3d53921507b1fabf497e8c1fa0d9
kiesen <creative77@gmail.com>
2019-08-12 15:58:41 +1000

    fix root node access

--- discord_sim/common/player/PlayerServer.gd
+++ discord_sim/common/player/PlayerServer.gd
@@ -14,7 +14,7 @@ func _on_NetworkUpdate():
 
 func set_user(usr):
 	yield(self, "ready")
-	var u = get_tree().get_node("Main/Backend/Data").users
+	var u = get_node("/root/Main/Backend/Data").users
 	var user : User = u[usr]
 	set_user_data(user.data)
 	rpc("set_user_data", user.data)
