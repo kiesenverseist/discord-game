64101e95405ec5b103eb4b04cc10b50166ebb572
kiesen <creative77@gmail.com>
2019-08-24 15:36:33 +1000

    point commands
    - point giving and donating
    - used discord_message_id where it is uncertain to be a DM
    - fixed avatar message to properly detect when no custom one is set
    -

--- discord_sim/server/backend/CommandHandler.gd
+++ discord_sim/server/backend/CommandHandler.gd
@@ -13,7 +13,9 @@ var commands = {
 	"update_user_leaderboard" : funcref(self, "update_user_leaderboard"),
 	"avatar" : funcref(self, "avatar"),
 	"token" : funcref(self, "token"),
-	"points" : funcref(self, "get_points")
+	"points" : funcref(self, "get_points"),
+	"give" : funcref(self, "give_user_points"),
+	"donate" : funcref(self, "give_team_points")
 }
 
 func _ready():
@@ -46,7 +48,7 @@ func handle_command(data):
 # admin
 func update_leaderboard(data, arguments):
 	if is_dm(data):
-		di.discord_message("This command cannot be used in DMs.", data["channel_name"], data["category_name"])
+		di.discord_message_id("This command cannot be used in DMs.", data["channel_id"])
 		return
 	
 	if not is_admin(data):
@@ -58,7 +60,7 @@ func update_leaderboard(data, arguments):
 
 func update_users(data, arguments):
 	if is_dm(data):
-		di.discord_message("This command cannot be used in DMs.", data["channel_name"], data["category_name"])
+		di.discord_message_id("This command cannot be used in DMs.", data["channel_id"])
 		return
 	
 	if not is_admin(data):
@@ -70,7 +72,7 @@ func update_users(data, arguments):
 
 func update_user_leaderboard(data, arguments):
 	if is_dm(data):
-		di.discord_message("This command cannot be used in DMs.", data["channel_name"], data["category_name"])
+		di.discord_message_id("This command cannot be used in DMs.", data["channel_id"])
 		return
 	
 	if not is_admin(data):
@@ -99,8 +101,8 @@ func avatar(data, arguments):
 		var url = (arguments[0] as String)
 		u[data["user_id"]].data["avatar_custom"] = url
 	
-	var av = u[data["user_id"]].data["avatar"] 
-	var av_c = u[data["user_id"]].data["avatar_custom"] + "\nUse the command `^avatar <url>` to change to <url>"
+	var av = u[data["user_id"]].data["avatar"]  + "\nUse the command `^avatar <url>` to change to <url>"
+	var av_c = u[data["user_id"]].data["avatar_custom"]
 	av = av_c  + "\nUse the command `^avatar -` to reset to default" \
 			if not av_c in ["-", "", null] else av
 	
@@ -111,13 +113,63 @@ func get_points(data, arguments):
 	var t = da.teams
 	var u = da.users
 
-	di.discord_message("Your team has %s points" % str(t[u[data["user_id"]].data["team"]].points)\
+	di.discord_message_id("Your team has %s points" % str(t[u[data["user_id"]].data["team"]].points)\
 			+ "\nYou have %s points" % str(u[data["user_id"]].points),\
-			data["channel_name"], data["category_name"])
+			data["channel_id"])
+
+func give_user_points(data, arguments):
+	var u = da.users
+	var giver = data["user_id"]
+	var reciever = str(int(arguments[0]))
+	
+	if not u.has(reciever):
+		di.discord_message_id("Recipient not found.", data["channel_id"])
+		return
+	
+	if not arguments[1].is_valid_integer():
+		di.discord_message_id("Invalid amount of points.", data["channel_id"])
+		return
+	
+	var pts = abs(int(arguments[1]))
+	
+	if pts > u[giver].points:
+		di.discord_message_id("You do not have sufficient points.", data["channel_id"])
+		return
+	
+	u[reciever].add_points(pts)
+	u[giver].add_points(-pts)
+	
+	di.discord_message_id("%s gave %s %s points." % \
+			[u[giver].data["mention"],u[reciever].data["mention"], str(pts)], data["channel_id"])
+
+func give_team_points(data, arguments):
+	var u = da.users
+	var t = da.teams
+	var giver = data["user_id"]
+	var team = u[giver].data["team"]
+	
+	if not arguments[0].is_valid_integer():
+		di.discord_message_id("Invalid amount of points.", data["channel_id"])
+		return
+	
+	var pts = abs(int(arguments[0]))
+	pts = pts - pts % 3
+	
+	if pts > u[giver].points:
+		di.discord_message_id("You do not have sufficient points.", data["channel_id"])
+		return
+	
+	t[team].add_points(pts/3)
+	u[giver].add_points(-pts)
+	
+	di.discord_message_id("%s donated %s points, giving %s %s more points" % \
+			[u[giver].data["mention"], str(pts), team, str(pts/3)], data["channel_id"])
+
 
 ## util functions
 func is_admin(data) -> bool:
 	return data["user_id"] in ["183363112882274305", "186298188800458752"]
 
 func is_dm(data) -> bool:
+	#remember to use discord_message_id if you are DMing or are unsure if you are
 	return data["is_dm"]
